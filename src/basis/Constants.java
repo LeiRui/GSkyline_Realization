f37ab6d03cb58d8aac2431736047a4321a6926ab
RuiLei <ruileisurprise@gmail.com>
2018-11-26 21:24:57 +0800

    init

--- src/basis/Constants.java
+++ src/basis/Constants.java
@@ -1,16 +1,12 @@
 package basis;
 
 public class Constants {
-    public static String anti_2 = "D:\\我的坚果云\\1\\博一上\\现代数据库概论\\大作业2\\project2\\datasets\\anti_2.txt";
-    public static String anti_2_save = "D:\\我的坚果云\\1\\博一上\\现代数据库概论\\大作业2\\project2\\datasets\\anti_2_save.txt";
     public static String my = "2d.txt";
     public static String my_save = "2d_save.txt";
     public static String my3 = "3d.txt";
     public static String my3_save = "3d_save.txt";
     public static String eg = "eg.txt";
     public static String eg_save = "eg_save.txt";
-    public static String inde_2 = "D:\\我的坚果云\\1\\博一上\\现代数据库概论\\大作业2\\project2\\datasets\\inde_2.txt";
-    public static String inde_4 = "D:\\我的坚果云\\1\\博一上\\现代数据库概论\\大作业2\\project2\\datasets\\inde_4.txt";
     public static String inde_8_short = "inde_8_short.txt";
     public static String inde_8_very_short = "inde_8_very_short.txt";
 
